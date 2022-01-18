fe819a3ff5ff5d5356d96456e12e770ee7b50af5
James Stephenson <90290035+jamesastephenson@users.noreply.github.com>
2022-01-18 15:59:14 -0500

    db connection works, added sorting, refactored
    Should add a radio button for ascending or descending order. Filter options (ie. only display over a certain transaction amount) could be a good idea as well

--- Data Table Site January 2022/submit.php
+++ Data Table Site January 2022/submit.php
@@ -8,71 +8,108 @@
     <link rel="stylesheet" type="text/css" href="./style.css">
 </head>
 
-<!-- MAY NEED TO MAKE A SEPARATE PHP FILE FOR WHEN FORM IS SUBMITTED, JUST COPY OVER BASE HTML LAYOUT-->
 <body>
     <div class="container">
         <div class="bannerSection">
             <h1>Data Table Site January 2022</h1>
         </div>
 
-        <!-- note: blank action attribute is essential for having this work on same page-->
         <form name="form" method="post" action="" class="controls">
             <h3 class="controlsTitle">Controls</h3>
 
-            <label>Cool! A Dropdown!</label>
-            <select name="option" onchange="this.form.submit();" method="post">
-                <option value="-" selected="selected">-</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
+            <label>Sort By: &nbsp;</label>
+            <select name="option" method="post">
+                <option value="Name">Name</option>
+                <option value="Email">Email</option>
+                <option value="Zip Code">Zip Code</option>
+                <option value="Customer Code">Customer Code</option>
+                <option value="Transaction Total">Transaction Total</option>
             </select>
+            <input type="submit" name="submit" onsubmit="this.form.submit()">
         </form>
 
         <div class="tableSection">
-            <!-- Dropdown selection test -->
-            <?php
-                $option = $_POST["option"];
-
-                if ($option == "-") {print "<h2>Please select an option.</h2>";}
-                else {print "<h2>You selected option ".$option."</h2>";}
-            ?>
-
-            <!-- THIS TABLE WILL BE REPLACED WITH DYNAMIC DATA -->
+            
             <table class="table">
                 <tr>
-                    <th>First Name</th>
-                    <th>Blood Type</th>
-                    <th>Bone Type</th>
-                    <th>Last Name</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Zip Code</th>
+                    <th>Customer Code</th>
+                    <th>Total</th>
                 </tr>
-                <tr class="oddRow">
-                    <td>Jeff</td>
-                    <td>A</td>
-                    <td>glass</td>
-                    <td>Jeffries</td>
-                </tr>
-                <tr class="evenRow">
-                    <td>Joan</td>
-                    <td>B</td>
-                    <td>paper</td>
-                    <td>Lark</td>
-                </tr>
-                <tr class="oddRow">
-                    <td>Jeof</td>
-                    <td>C</td>
-                    <td>broth</td>
-                    <td>Joffreys</td>
-                </tr>
-                <tr class="evenRow">
-                    <td>Joober</td>
-                    <td>????</td>
-                    <td>????</td>
-                    <td>Jooberov</td>
-                </tr>
+            <?php
+                // assign vars for server connection
+                $serverName = "localhost";  $dbUsername = "root";
+                $dbPassword = "";  $dbName = "january_2022";
+                $connect = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+
+                // connection is working
+                if (!$connect) {
+                    die("Connection failed: ".mysqli_connect_error());
+                } else {
+                    $option = $_POST["option"];
+                }
+
+                function sortData($selection) {
+                    // reiterating vars for allowing additional selection
+                    $serverName = "localhost";  $dbUsername = "root";
+                    $dbPassword = "";  $dbName = "january_2022";
+                    $connect = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+
+                    //lmao
+                    if ($selection == "-") {echo '<h2 class="tableTitle red">Please select an option.</h2>';}
+                    else if ($selection == "Name") {$sortVar = "name";}
+                    else if ($selection == "Email") {$sortVar = "email";}
+                    else if ($selection == "Zip Code") {$sortVar = "postalZip";}
+                    else if ($selection == "Customer Code") {$sortVar = "alphanumeric";}
+                    else if ($selection == "Transaction Total") {$sortVar = "currency";}
+                    else {echo '<h2 class="tableTitle red">What did you do?</h2>';}
+
+                    $sqlRequest = "
+                    SELECT * 
+                    FROM users 
+                    ORDER BY ".$sortVar;  // add DESC to descend order
+
+                    // -> is used to call a method or access a property on the object of a class
+                    // => is used to assign values to keys of an array
+                    $result = $connect->query($sqlRequest);
+                    print '<h2 class="tableTitle">Sorting by '.$selection.'</h2>';
+
+                    if ($result->num_rows > 0) {
+                        $rowCount = 1;
+                        while ($row = $result->fetch_assoc()) {
+                            if ($rowCount % 2 == 0 ) {
+                                echo '<tr class="evenRow">
+                                    <td>'.$row["name"].'</td>
+                                    <td>'.$row["email"].'</td>
+                                    <td>'.$row["postalZip"].'</td>
+                                    <td>'.$row["alphanumeric"].'</td>
+                                    <td>'.$row["currency"].'</td>
+                                </tr>';
+                            }
+                            if ($rowCount % 2 != 0 ) {
+                                echo '<tr class="oddRow">
+                                    <td>'.$row["name"].'</td>
+                                    <td>'.$row["email"].'</td>
+                                    <td>'.$row["postalZip"].'</td>
+                                    <td>'.$row["alphanumeric"].'</td>
+                                    <td>'.$row["currency"].'</td>
+                                </tr>';
+                            }
+                            $rowCount += 1;
+                        }
+                    } else {
+                        echo "no results";
+                    }
+                    $connect->close();
+                }
+                
+                sortData($option);
+            ?>
             </table>
         </div>
+        <footer>This is a footer</footer>
     </div>
-    <footer>This is a footer</footer>
 </body>
 </html>
